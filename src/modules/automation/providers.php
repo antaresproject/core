913e38fdada1c1cebe6fd76a0fe1d93dccffa80f
mounstone <lukasz.cirut@gmail.com>
2017-05-16 15:30:41 +0200

    FIX: remove modules directory

--- src/modules/automation/providers.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    Antares\Automation\AutomationServiceProvider::class,
-];
--- src/modules/logger/providers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    Antares\Logger\LoggerServiceProvider::class,
-    Antares\Logger\RequestLoggerServiceProvider::class,
-];
