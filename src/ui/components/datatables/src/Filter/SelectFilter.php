52e6491ac8343fbfabfc3385b75a975054e953c1
mountstone <lukasz.cirut@gmail.com>
2017-10-31 09:23:40 +0100

    ADD: frontend integration

--- src/ui/components/datatables/src/Filter/SelectFilter.php
+++ src/ui/components/datatables/src/Filter/SelectFilter.php
@@ -38,6 +38,13 @@ class SelectFilter extends AbstractFilter
     protected $placeholder = null;
 
     /**
+     * Type of filter for frontend issues
+     *
+     * @var String 
+     */
+    protected $type = 'status';
+
+    /**
      * Values getter
      * 
      * @return array
@@ -49,7 +56,8 @@ class SelectFilter extends AbstractFilter
         if (is_null($params) or ! isset($params['column']) or ! $params['column'] == $this->column) {
             return [];
         }
-        if (empty($params['value'])) {
+
+        if (empty((array) $params['value'])) {
             $params = $this->session->get($uri);
             unset($params[get_called_class()]);
             $this->session->put($uri, $params);
@@ -68,12 +76,16 @@ class SelectFilter extends AbstractFilter
     {
         $subview = $this->render();
         $values  = $this->getValues();
-        if (empty($values)) {
+
+        if (empty((array) $values)) {
             return '';
         }
+
         $name      = str_replace('%value', implode(', ', array_only($this->options(), $values)), trans($this->pattern));
         $classname = get_called_class();
+
         return view('antares/automation::admin.partials._deleted')->with([
+                    'type'      => $this->type,
                     'column'    => $this->column,
                     'instance'  => $this,
                     'route'     => uri(),
@@ -88,13 +100,16 @@ class SelectFilter extends AbstractFilter
      */
     public function render()
     {
+        $classname   = get_called_class();
         publish('automation', ['js/automation_status_filter.js']);
         $selected    = $this->getValues();
         $placeholder = is_null($this->placeholder) ? trans('antares/foundation::messages.select_placeholder_default', ['name' => strtolower($this->name)]) : $this->placeholder;
-        return view('datatables-helpers::partials._filter_select_multiple', [
+        return view('datatables-helpers::partials._filter_select', [
+                    'type'        => $this->type,
                     'options'     => $this->options(),
                     'column'      => $this->column,
                     'placeholder' => $placeholder,
+                    'classname'   => $classname,
                     'selected'    => $selected
                 ])->render();
     }
