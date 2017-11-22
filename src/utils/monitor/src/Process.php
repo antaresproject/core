691dc280b5678afa4376c99e5115fb62deea29a9
Marcin Kozak <marcin.ko@modulesgarden.com>
2017-11-22 13:32:42 +0100

    ADD: monitor for checking commands running in background

---
+++ src/utils/monitor/src/Process.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Antares\Monitor;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class Process implements Arrayable {
+
+    /**
+     * @var string
+     */
+    protected $command;
+
+    /**
+     * @var ProcessInfo|null
+     */
+    protected $info;
+
+    /**
+     * Process constructor.
+     * @param string $command
+     * @param ProcessInfo|null $info
+     */
+    public function __construct(string $command, ProcessInfo $info = null) {
+        $this->command  = $command;
+        $this->info     = $info;
+    }
+
+    /**
+     * Returns the process command.
+     *
+     * @return string
+     */
+    public function getCommand() : string {
+        return $this->command;
+    }
+
+    /**
+     * Sets info to the process.
+     *
+     * @param ProcessInfo $info
+     */
+    public function setInfo(ProcessInfo $info) {
+        $this->info = $info;
+    }
+
+    /**
+     * Returns the process info if the process is running.
+     *
+     * @return ProcessInfo|null
+     */
+    public function getInfo() : ?ProcessInfo {
+        return $this->info;
+    }
+
+    /**
+     * Checks if the process is running.
+     *
+     * @return bool
+     */
+    public function isRunning() : bool {
+        return $this->info instanceof ProcessInfo;
+    }
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray() : array {
+        return [
+            'command'   => $this->getCommand(),
+            'running'   => $this->isRunning(),
+            'info'      => $this->getInfo() ? $this->getInfo()->toArray() : null,
+        ];
+    }
+}
